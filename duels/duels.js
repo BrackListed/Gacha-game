5e6120ce0495796aa8d140b4183798a568756d72
BrackListed <cameta8900@gmail.com>
2026-05-04 17:41:33 +0800

    Added UI interface for selecting characters and starting the game

--- duels/duels.js
+++ duels/duels.js
@@ -1,1 +1,13 @@
 let storedCharacters = JSON.parse(localStorage.getItem("stored-characters")) || []
+let selectionEl = document.querySelectorAll(".selection")
+
+
+selectionEl.forEach((chosableCharacters, index) => {
+    chosableCharacters.textContent = storedCharacters[index].name
+});
+
+
+
+    //make it so that when this thing loops thru all the buttons, it also changes each innerhmtl based on the storedcharacters, so basically the first button
+    //characterthings[0] = storedcharacters[0] so yeha
+    //I'm only accessing the array, not the object, let's try to access it
