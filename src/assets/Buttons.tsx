1bca0a15b31f2ca825beb723b741c8f09257faab
BrackListed <cameta8900@gmail.com>
2026-05-16 18:43:40 +0800

    Added a header with 3 buttons: Storage, Start, Player Duels

---
+++ src/assets/Buttons.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from "react";
+import { motion } from "motion/react"
+
+type ButtonProp = {
+    children: ReactNode
+}
+
+export function Buttons({children}: ButtonProp) {
+    
+    return(
+        <motion.button className="text-zinc-50 border-2 border-zinc-50 bg-transparent hover:backdrop-blur-xs w-52 py-5 px-7 rounded-lg hover:cursor-pointer"
+        whileHover={{
+            scale: 1.15,
+            boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+            textShadow: "0px 0px 8px rgb(255, 255, 255)",
+        }}
+        
+        >{children}</motion.button>
+    )
+}
